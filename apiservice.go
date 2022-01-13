216e41bf2c19033e3a7534cd1c82fcde65932ee1
charlie <marmot00@gmail.com>
2022-01-13 10:03:20 +0800

    add /block route

--- apiservice.go
+++ apiservice.go
@@ -19,11 +19,35 @@ type APIService struct {
 
 func (s APIService) ListenAndServe(addr string) {
 	r := gin.Default()
+	r.GET("/block", s.blockHandler)
 	r.GET("/blocks", s.blocksHandler)
 	err := r.Run(addr)
 	if err != nil {
 		log.Fatal("failed to run http server, ", err)
 	}
+}
+
+func (s APIService) blockHandler(c *gin.Context) {
+	idRaw := c.Query("id")
+
+	id, err := strconv.Atoi(idRaw)
+	if err != nil || id < 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "invalid ID",
+		})
+		return
+	}
+
+	block, err := s.indexer.GetBlock(uint64(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": fmt.Sprintf("error: %v", err),
+		})
+		return
+	}
+
+	// todo: return with transactions
+	c.JSON(http.StatusOK, block)
 }
 
 func (s APIService) blocksHandler(c *gin.Context) {
